dfe1774501d7bf244b9bc5406a98585ace4fc39b
kartik7603 <kartik76037242@gmail.com>
2026-03-20 14:26:39 +0530

    Add files via upload

---
+++ class2_different birthday dates.py
@@ -0,0 +1,13 @@
+# storing 5 different birthday dates in 5 variables
+birthday_1 = "January 15"
+birthday_2 = "february 25"
+birthday_3 = "March 21"
+birthday_4 = "May 25"
+birthday_5 = "june 11"
+
+# printing the birthday to verify they are stored
+print("birthday 1:", birthday_1)
+print("birthday 2:", birthday_2)
+print("birthday 3:", birthday_3)
+print("birthday 4:", birthday_4)
+print("birthday 5:", birthday_5)
